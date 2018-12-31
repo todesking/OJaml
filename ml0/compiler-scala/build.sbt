9cb62c8d2fb1b44a96a8f65a3713acde1d26de87
todesking <discommunicative@gmail.com>
2018-12-31 20:36:06 +0900

    First test(empty class) passed

--- ml0/compiler-scala/build.sbt
+++ ml0/compiler-scala/build.sbt
@@ -1,3 +1,3 @@
 scalaVersion := "2.12.8"
 
-
+libraryDependencies += "org.ow2.asm" % "asm" % "7.0"
--- ml0/compiler-scala/src/main/scala/Compiler.scala
+++ ml0/compiler-scala/src/main/scala/Compiler.scala
@@ -1,9 +1,28 @@
 package com.todesking.ojaml.ml0.compiler.scala
 
 import java.nio.file.Path
+import java.nio.file.Files
 
-class Compiler(out: Path) {
+import org.objectweb.asm
+
+import asm.{ Opcodes => op }
+
+class Compiler(baseDir: Path) extends asm.Opcodes {
   def compile(files: Seq[Path]): Unit = {
+    val cw = new asm.ClassWriter(0)
+    cw.visit(
+      op.V1_8,
+      op.ACC_PUBLIC,
+      "test/ml0/Empty",
+      null,
+      "java/lang/Object",
+      Array())
+    cw.visitEnd()
+    val data = cw.toByteArray()
+    val packageDir = baseDir.resolve("test/ml0")
+    val out = packageDir.resolve("Empty.class")
+    Files.createDirectories(packageDir)
+    Files.write(out, data)
   }
 }
 
