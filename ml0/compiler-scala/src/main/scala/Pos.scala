a25bf7b97a13d908d93200d28a50f3d8eccfdeee
todesking <discommunicative@gmail.com>
2020-03-15 00:01:47 +0900

    No more HasPos

--- ml0/compiler-scala/src/main/scala/Pos.scala
+++ ml0/compiler-scala/src/main/scala/Pos.scala
@@ -4,5 +4,4 @@ case class Pos(location: String, line: Int, col: Int) {
   override def toString = s"$location:$line:$col"
 }
 object Pos {
-  def fill[A <: HasPos](v: A, p: Pos): A = { v.fillPos(p); v }
 }
