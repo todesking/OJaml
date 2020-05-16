e10a3860e78e2e32d9742ff74014811bbc966ac6
todesking <discommunicative@gmail.com>
2020-05-16 20:45:43 +0900

    Predef/Runtime: refactor + add basic equality functions

--- ml0/lib-native/src/main/java/com/todesking/ojaml/ml0/runtime/Bool.java
+++
@@ -1,7 +0,0 @@
-package com.todesking.ojaml.ml0.runtime;
-
-public class Bool {
-  public static boolean or(boolean l, boolean r) { return l || r; }
-  public static boolean and(boolean l, boolean r) { return l && r; }
-}
-
---
+++ ml0/lib-native/src/main/java/com/todesking/ojaml/ml0/runtime/Runtime.java
@@ -0,0 +1,22 @@
+package com.todesking.ojaml.ml0.runtime;
+
+public class Runtime {
+  public static boolean stringEq(java.lang.String l, java.lang.String r) {
+    if(l == null) return r == null;
+    else return l.equals(r);
+  }
+
+  public static int intAdd(int l, int r) { return l + r; }
+  public static int intSub(int l, int r) { return l - r; }
+  public static int intMul(int l, int r) { return l * r; }
+  public static int intDiv(int l, int r) { return l / r; }
+  public static int intMod(int l, int r) { return l % r; }
+  public static boolean intEq(int l, int r) { return l == r; }
+  public static boolean intLt(int l, int r) { return l < r; }
+  public static boolean intGt(int l, int r) { return l > r; }
+
+  public static boolean boolOr(boolean l, boolean r) { return l || r; }
+  public static boolean boolAnd(boolean l, boolean r) { return l && r; }
+  public static boolean boolEq(boolean l, boolean r) { return l == r; }
+}
+
